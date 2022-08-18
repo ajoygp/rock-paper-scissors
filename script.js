fdd2b20c907fb5335950e159cbb36d60362889db
ajoygp <despajoy@gmail.com>
2022-08-18 22:38:20 +0530

    finish working ui

--- script.js
+++ script.js
@@ -4,70 +4,82 @@ wins={"rock":"scissors",
      "scissors": "paper"
 };
 
+let comScore=0;
+let userScore=0;
+let computerScoreElm=document.querySelector(".computer-score")
+let computerChoiceElm=document.querySelector(".computer-choice")
+let userChoiceElm=document.querySelector(".user-choice")
+let userScoreElm=document.querySelector(".user-score")
+let result=document.querySelector(".result")
+let roundResult=document.querySelector(".round-result")
+
+let choices=document.querySelectorAll(".choice");
+choices.forEach(choice => {
+    choice.addEventListener("click",gameRound);
+});
+
 
 function getComputerChoice (){
     choice=Math.floor(Math.random()*3);
     return(hand[choice]);
 }
 
-
-
-function getUserChoice(){
-    let choice;
-        while(!hand.includes(choice)){
-            choice=prompt("Enter Your Choice").toLowerCase();    
-        }
-    return(choice);
-
-}
-
-
 function gameRound(){
+
+    userChoice= this.getAttribute("data-key");
     computerChoice=getComputerChoice();
-    let userChoice= getUserChoice();
+    computerChoiceElm.innerText=computerChoice;
+    userChoiceElm.innerText=userChoice;
+    userScoreElm.innerText=userScore;
+    computerScoreElm.innerText=comScore;
 
     if(computerChoice==userChoice){
-        console.log(`Draw! Both ${userChoice}`);
-    }
-    else if(wins[userChoice]==computerChoice){
-        console.log(`You Win! ${userChoice} beats ${computerChoice}`)
-        return(1);
-    }
-    else if (wins[computerChoice]==userChoice){
-        console.log(`You Lose! ${computerChoice} beats ${userChoice}`)
-        return(2);
+       
+     roundResult.innerText=`Draw! Both ${userChoice}`;
     }
 
-  
+    else if(wins[userChoice]==computerChoice){
+        userScore+=1;
+        userScoreElm.innerText=userScore;
+        roundResult.innerText=`You Win! ${userChoice} beats ${computerChoice}`
+        
+    }
+    else if (wins[computerChoice]==userChoice){
+        
+        comScore+=1;
+        computerScoreElm.innerText=comScore;
+        roundResult.innerText=`You Lose! ${computerChoice} beats ${userChoice}`
+    
+    }
+    winner(userScore,comScore);
 
 }
 
 function winner(one, two){
-    if(one==two){
-        return ("Its a tie");
-    }
-    else{
-        return (one>two? "You win":"Computer wins");
-    }
+  
+     if (one>=5){
+        result.innerText ="You win";
+        choices.forEach(choice => {
+            choice.removeEventListener("click",gameRound);
+        });
+     }
+     else if (two>=5){
+        result.innerText = "Computer wins";
+        choices.forEach(choice => {
+            choice.removeEventListener("click",gameRound);
+        });
+     }
+        
 }
 
 function game(){
     
-    let comScore=0;
-    let userScore=0;
+    
     for(let i=0; i<5;i++){
         
-        score=gameRound();
-        if(score==1){
-            userScore++;
-        }
-        else if(score==2){
-            comScore++;
-        }
-        score=0;
     }
     
     console.log(`You: ${userScore}| Computer: ${comScore} \n ${winner(userScore,comScore)}`)
 }
 
-game();
+
